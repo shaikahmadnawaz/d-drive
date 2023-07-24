a38ec766df45c1f2324aae72687d13d631576154
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-07-25 01:23:54 +0530

    smart contract completed

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -22,7 +22,35 @@ contract Upload{
     value[_user].push(url);
   }
 
-  function allow(address user) external{
-    ownership[msg.sender][user] = true;
+  function allow(address user) external {
+    ownership[msg.sender][user]=true; 
+    if(previousData[msg.sender][user]){
+      for(uint i=0;i<accessList[msg.sender].length;i++){
+        if(accessList[msg.sender][i].user==user){
+          accessList[msg.sender][i].access=true; 
+        }
+      }
+    }else{
+      accessList[msg.sender].push(Access(user,true));  
+      previousData[msg.sender][user]=true;  
+    }  
+  }
+
+  function disallow(address user) public{
+    ownership[msg.sender][user]=false;
+    for(uint i=0;i<accessList[msg.sender].length;i++){
+      if(accessList[msg.sender][i].user==user){ 
+        accessList[msg.sender][i].access=false;  
+      }
+    }
+  }
+
+  function display(address _user) external view returns(string[] memory){
+    require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
+    return value[_user];
+  }
+
+  function shareAccess() public view returns(Access[] memory){
+    return accessList[msg.sender];
   }
 }
